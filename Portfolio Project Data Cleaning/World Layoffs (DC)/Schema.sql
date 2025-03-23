abf8d4a35ae79151c856056c42f4abaf507c88b4
Rimsha Siddiqui <163817988+rimshasiddiqui68@users.noreply.github.com>
2025-03-23 21:20:32 +0530

    Update Schema.sql

--- Portfolio Project Data Cleaning/World Layoffs (DC)/Schema.sql
+++ Portfolio Project Data Cleaning/World Layoffs (DC)/Schema.sql
@@ -1,1 +1,16 @@
+-- Create a Database "Layoffs"
+CREATE DATABASE IF NOT EXISTS Layoffs;
+USE Layoffs;
+
+--The first step in cleaning the dataset is to create a **staging table** where all transformations and cleaning operations will be performed.
+--This ensures that the original raw data remains intact in case we need to reference it later.
+
+Create table layoffs_staging  
+like layoffs;
+
+-- Inserting the values from original(layoffs) table.
+INSERT layoffs_staging
+SELECT * FROM 
+layoffs;
+
 
