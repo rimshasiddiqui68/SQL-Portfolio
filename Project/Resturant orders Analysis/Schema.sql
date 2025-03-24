52f5f5d1549afa2bad5631affc98630173f99e0e
Rimsha Siddiqui <163817988+rimshasiddiqui68@users.noreply.github.com>
2025-03-25 03:47:45 +0530

    Update Schema.sql

--- Project/Resturant orders Analysis/Schema.sql
+++ Project/Resturant orders Analysis/Schema.sql
@@ -13,3 +13,23 @@ SELECT * FROM menu_items;
 INSERT order_details1
 SELECT * FROM 
 order_details;
+
+-- checking the duplicates 
+select `ï»¿menu_item_id` ,count(*)
+from menu_items1
+group by `ï»¿menu_item_id`
+having count(*)>1 ;
+
+select `ï»¿order_details_id` ,count(*)
+from order_details1
+group by `ï»¿order_details_id`
+having count(*) > 1;
+-- -- No duplicate found
+
+-- LETS change the header of both table
+ALTER table menu_items1
+CHANGE 
+`ï»¿menu_item_id` menu_item_id INT;
+ALTER TABLE order_details1
+CHANGE
+`ï»¿order_details_id` order_details_id INT;
