c536ac2f7f1bea47af514a1e802ddbd888302365
Rimsha Siddiqui <163817988+rimshasiddiqui68@users.noreply.github.com>
2025-03-25 03:46:29 +0530

    Update Schema.sql

--- Project/Resturant orders Analysis/Schema.sql
+++ Project/Resturant orders Analysis/Schema.sql
@@ -1,1 +1,15 @@
-Create 
+-- Create a database if does not exist
+CREATE DATABASE resturant_orders;
+
+CREATE TABLE menu_items1
+LIKE menu_items;
+
+CREATE TABLE order_details1
+LIKE order_details;
+
+INSERT menu_items1
+SELECT * FROM menu_items;
+
+INSERT order_details1
+SELECT * FROM 
+order_details;
